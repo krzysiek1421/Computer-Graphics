18d543b7cf47394278ce01044f24bb381545d857
Krzysztof Biel <68143000+krzysiek1421@users.noreply.github.com>
2022-04-02 18:31:52 +0200

    Update TransformedShapes.java

--- Lab_01/TransformedShapes.java
+++ Lab_01/TransformedShapes.java
@@ -49,17 +49,6 @@ public class TransformedShapes extends JPanel {
         g2 = (Graphics2D)g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // TODO Draw the required image, using ONLY the four methods defined above,
-        // along with g2.setColor, g1.scale, g2.translate, and g2.rotate.
-
-        /* ----------------------------------------------------------------------- */
-
-        // REMOVE THE FOLLOWING CODE, which draws a big red X in the upper right quadrant,
-        // and insert your own code to draw the required pictures in the four quadrants.
-
-        // The next two line scale the X to be twice the original size
-        // and then moves the center of the X from (0,0) to (450,150).
-
         g2.translate(300,300);
         g2.setColor(Color.BLUE);
         g2.scale(1.75,0.75);
@@ -71,26 +60,7 @@ public class TransformedShapes extends JPanel {
         g2.translate(375, 280);
         g2.rotate(Math.PI);
         triangle();
-        // The next three lines draw a tilted rectangle centered at (0,0).
-
-//        g2.rotate(Math.PI/4);
-//        g2.scale(1,0.25);
-//        square();
-
-        // The next two lines undo the previous scale and rotation.  (Can't use resetTransform()
-        // here because that would also eliminate the first translate and scale.)
-
-//        g2.scale(1,4);
-//        g2.rotate(-Math.PI/4);
-
-        // The next three lines draw a rectangle tilted in the opposite direction.
-
-//        g2.rotate(-Math.PI/4);
-//        g2.scale(1,0.25);
-//        square();
-
-        // reset the transform, so changes don't apply to subsequent drawing
-
+      
         resetTransform();
 
         /* ----------------------------------------------------------------------- */
